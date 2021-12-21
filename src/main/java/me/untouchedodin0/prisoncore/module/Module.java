af89483667f256586ea137e3d895425b01d25ffe
UntouchedOdin0 <kylehicksuk@gmail.com>
2021-12-21 21:09:43 +0000

    setupConfig method saveConfig method
    onEnable method
    onDisable method
    onLoad method

--- src/main/java/me/untouchedodin0/prisoncore/module/Module.java
+++ src/main/java/me/untouchedodin0/prisoncore/module/Module.java
@@ -5,9 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 import me.untouchedodin0.prisoncore.PrisonCore;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandMap;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.logging.Logger;
 
@@ -28,14 +31,64 @@ public class Module {
     private File configFile;
     private FileConfiguration config;
     private Logger logger;
+    private CommandMap commandMap;
 
     public Module() {
         prisonCore = PrisonCore.getInstance();
         final Field bukkitCommandMap;
         try {
             bukkitCommandMap = Bukkit.getServer().getClass().getDeclaredField("commandMap");
-        } catch (NoSuchFieldException exception) {
+            bukkitCommandMap.setAccessible(true);
+            commandMap = (CommandMap) bukkitCommandMap.get(Bukkit.getServer());
+        } catch (NoSuchFieldException | IllegalAccessException exception) {
             exception.printStackTrace();
         }
+        this.logger = Logger.getLogger(getName());
+    }
+
+    void setupConfig() {
+        dataFolder = new File(file, name);
+        boolean created = dataFolder.mkdirs();
+
+        System.out.println("dataFolder: " + dataFolder);
+        configFile = new File(dataFolder, "config.yml");
+        try {
+            boolean createdFile = configFile.createNewFile();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+        config = YamlConfiguration.loadConfiguration(configFile);
+    }
+
+    public void saveConfig() {
+        try {
+            config.save(configFile);
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    /*
+     *  This will be called when the module is enabled
+     */
+
+    public void onEnable() {
+
+    }
+
+    /*
+        This will be called when the module gets disabled
+     */
+
+    public void onDisable() {
+
+    }
+
+    /*
+        This will be called when the module first gets loaded
+     */
+
+    public void onLoad() {
+
     }
 }
